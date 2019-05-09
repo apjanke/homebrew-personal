7cf7a144f6829dd7ccd1d980ec381efeea53a665
Andrew Janke <andrew@apjanke.net>
2019-05-08 22:42:05 -0400

    Add nheko-reborn cask

---
+++ Casks/nheko-reborn.rb
@@ -0,0 +1,11 @@
+cask 'nheko-reborn' do
+  version '0.6.3'
+  sha256 'fe6f5effbcca452e09254d1f1eda41fa806449b14cc998332cd98869e5ef0c61'
+
+  url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version}/nheko-v#{version}.dmg"
+  appcast 'https://github.com/Nheko-Reborn/nheko/releases.atom'
+  name 'Nheko'
+  homepage 'https://github.com/Nheko-Reborn/nheko'
+
+  app 'Nheko.app'
+end
