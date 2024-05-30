bd11fd6ced2a524f5c531f53fdcccc3a257774a2
Andrew Janke <andrew@apjanke.net>
2024-05-29 22:30:32 -0400

    nheko-reborn: remove deprecated appcast stanza

--- Casks/nheko-reborn.rb
+++ Casks/nheko-reborn.rb
@@ -3,7 +3,6 @@ cask 'nheko-reborn' do
   sha256 'fe6f5effbcca452e09254d1f1eda41fa806449b14cc998332cd98869e5ef0c61'
 
   url "https://github.com/Nheko-Reborn/nheko/releases/download/v#{version}/nheko-v#{version}.dmg"
-  appcast 'https://github.com/Nheko-Reborn/nheko/releases.atom'
   name 'Nheko'
   homepage 'https://github.com/Nheko-Reborn/nheko'
 
